5bc4e7c55d05b3f7660158a6cd7b1b128f6180a6
rodrigo olmos canovas <rodrigo.olmos.canovas@gmail.com>
2024-07-30 19:17:16 +0200

    codigo mejorado para vitis

--- C_code/predict.c
+++ C_code/predict.c
@@ -3,17 +3,16 @@
 void predict(struct tree trees[], float features[], uint8_t *prediction) {
     int32_t sum = 0;
 
-    for (int t = 0; t < N_TREES; t++) {
+    trees_loop:for (int t = 0; t < N_TREES; t++) {
         int node_index = 0;
+        int feature_index;
+        float threshold;
 
         while (trees[t].leaf_or_node[node_index] == 1) {
-            int feature_index = trees[t].feature_index[node_index];
-            float threshold = trees[t].node_leaf_value[node_index];
-            if (features[feature_index] <= threshold) {
-                node_index += 1;
-            } else {
-                node_index = trees[t].next_node_right_index[node_index];
-            }
+            feature_index = trees[t].feature_index[node_index];
+            threshold = trees[t].node_leaf_value[node_index];
+
+            node_index = (features[feature_index] <= threshold) ? node_index + 1 : trees[t].next_node_right_index[node_index];
         }
         sum += trees[t].node_leaf_value[node_index];
     }
