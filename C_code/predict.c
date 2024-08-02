cf04d297df209d9175911b305652de061b68d74e
rodrigo olmos canovas <rodrigo.olmos.canovas@gmail.com>
2024-08-02 14:12:04 +0200

    custom float cmp added and pragmas

--- C_code/predict.c
+++ C_code/predict.c
@@ -1,10 +1,35 @@
 #include "predict.h"
 
+uint8_t cmpf(float a, float b) {
+    // Convert the floats to unsigned 32-bit integers
+    uint32_t int_a = *(uint32_t*)&a;
+    uint32_t int_b = *(uint32_t*)&b;
+
+    // Extract the sign bits, exponents, and mantissas
+    uint8_t sign_a = int_a >> 31;
+    uint8_t sign_b = int_b >> 31;
+    uint32_t abs_a = int_a & 0x7FFFFFFF;
+    uint32_t abs_b = int_b & 0x7FFFFFFF;
+
+    // Compare the sign bits
+    if (sign_a > sign_b) {
+        return 1;
+    }
+
+    // Compare the absolute values considering the sign
+    uint8_t temp = (abs_a > abs_b);
+    return (!sign_a) ? !temp : temp;
+}
+
 void predict(float_int_union node_leaf_value[N_TREES][N_NODE_AND_LEAFS],
             uint8_t compact_data[N_TREES][N_NODE_AND_LEAFS],
             uint8_t next_node_right_index[N_TREES][N_NODE_AND_LEAFS],
             float features[N_FEATURE], uint8_t *prediction){
 
+	#pragma HLS INTERFACE mode=s_axilite port=features
+	#pragma HLS INTERFACE mode=s_axilite port=next_node_right_index
+	#pragma HLS INTERFACE mode=s_axilite port=compact_data
+	#pragma HLS INTERFACE mode=s_axilite port=node_leaf_value
 	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=next_node_right_index
 	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=compact_data
 	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=node_leaf_value
@@ -36,7 +61,7 @@ void predict(float_int_union node_leaf_value[N_TREES][N_NODE_AND_LEAFS],
             node_left = node_index + 1;
             node_right = next_node_right_index[t][node_index];
 
-            node_index = (features_local[feature_index] <= threshold) ? node_left : node_right;
+            node_index = cmpf(features_local[feature_index], threshold) ? node_left : node_right;
 
             done = (compact_data[t][node_index] & 0x80) == 0 ? 1 : 0;
 
