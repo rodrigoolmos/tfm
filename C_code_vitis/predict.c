42503488ca18196604ddb87cbfd2aab995fe307d
rodrigo olmos canovas <rodrigo.olmos.canovas@gmail.com>
2024-08-04 11:48:58 +0200

    problemas de las interfaces resuelto

--- C_code_vitis/predict.c
+++ C_code_vitis/predict.c
@@ -13,30 +13,25 @@ void predict(float_int_union node_leaf_value[N_TREES][N_NODE_AND_LEAFS],
             uint8_t next_node_right_index[N_TREES][N_NODE_AND_LEAFS],
             float features[N_FEATURE], uint8_t *prediction){
 
+	#pragma HLS TOP name=predict
+
 	#pragma HLS INTERFACE mode=s_axilite port=prediction bundle=prediction
 	#pragma HLS INTERFACE mode=s_axilite port=features bundle=features
 	#pragma HLS INTERFACE mode=s_axilite port=next_node_right_index bundle=next_node_right_index
 	#pragma HLS INTERFACE mode=s_axilite port=compact_data bundle=compact_data
 	#pragma HLS INTERFACE mode=s_axilite port=node_leaf_value bundle=node_leaf_value
-	#pragma HLS INTERFACE s_axilite port=return bundle=results
-/*
+	#pragma HLS INTERFACE s_axilite port=return bundle=control
+
 	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=next_node_right_index
 	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=compact_data
 	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=node_leaf_value
-	#pragma HLS ARRAY_PARTITION dim=1 factor=N_FEATURE/2 type=cyclic variable=features
-*/
+	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=features
 
 	int32_t sum = 0;
     int32_t leaf_value;
-    float features_local[N_FEATURE];
-
-    coppy_features:for (int f = 0; f < N_FEATURE; f++) {
-        features_local[f] = features[f];
-    }
-
 
     trees_loop:for (int t = 0; t < N_TREES; t++) {
-	//#pragma HLS UNROLL
+	#pragma HLS UNROLL
 
         uint8_t node_index = 0;
         uint8_t node_right;
@@ -52,7 +47,7 @@ void predict(float_int_union node_leaf_value[N_TREES][N_NODE_AND_LEAFS],
             node_left = node_index + 1;
             node_right = next_node_right_index[t][node_index];
 
-            node_index = cmpf(features_local[feature_index], threshold) ? node_left : node_right;
+            node_index = cmpf(features[feature_index], threshold) ? node_left : node_right;
 
             done = (compact_data[t][node_index] & 0x80) == 0 ? 1 : 0;
 
