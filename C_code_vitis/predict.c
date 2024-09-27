c96400dda53ed72f70e2c6d15c0e7d6eec8a87f5
rodrigo olmos <tic.rodrigo.olmos@gmail.com>
2024-09-27 11:21:22 +0200

    added internal ping pong buffer in the Vitis ip aparently working

--- C_code_vitis/predict.c
+++ C_code_vitis/predict.c
@@ -9,7 +9,9 @@ void predict(uint64_t tree[N_TREES][N_NODE_AND_LEAFS],
             int32_t ping_pong){
 
     int32_t leaf_value;
-    float local_features[N_FEATURE];
+    static int8_t local_ping_pong;
+    float local_features_ping[N_FEATURE];
+    float local_features_pong[N_FEATURE];
 
 	#pragma HLS TOP name=predict
 	#pragma HLS INTERFACE mode=bram port=prediction_ping
@@ -21,45 +23,91 @@ void predict(uint64_t tree[N_TREES][N_NODE_AND_LEAFS],
 	#pragma HLS INTERFACE mode=s_axilite port=ping_pong bundle=control
 	#pragma HLS INTERFACE s_axilite port=return bundle=control
 	#pragma HLS ARRAY_PARTITION dim=1 factor=N_TREES type=block variable=tree
-	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=local_features
+	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=local_features_ping
+	#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=local_features_pong
 
+    if (!local_ping_pong){
+        coppy_loop_pong_init: for (int i = 0; i < N_FEATURE; i++){
+            local_features_pong[i] = ping_pong & 0x01 ? bram_features_ping[0][i] : bram_features_pong[0][i];
+        }
+    }else{
+        coppy_loop_ping_init: for (int i = 0; i < N_FEATURE; i++){
+            local_features_ping[i] = ping_pong & 0x01 ? bram_features_ping[0][i] : bram_features_pong[0][i];
+        }
+    }
+    
     burst_loop:for (int j = 0; j < *features_burst_length; j++){
 	#pragma HLS loop_tripcount min=1 max=MAX_BURST_FEATURES
-        // preguntar como hacer que sea ping pong buffer
-        coppy_loop:for (int i = 0; i < N_FEATURE; i++){
-            local_features[i] = ping_pong & 0x01 ? bram_features_ping[j][i] : bram_features_pong[j][i];
-        }
-        
+
 	    int32_t sum = 0;
 
-        trees_loop:for (int t = 0; t < N_TREES; t++){
-        #pragma HLS UNROLL factor=N_TREES
+        if (local_ping_pong){
 
-            uint8_t node_index = 0;
-            uint8_t node_right;
-            uint8_t node_left;
-            uint8_t feature_index;
-            float threshold;
-            tree_data tree_data;
+            trees_loop_ping: for (int t = 0; t < N_TREES; t++){
+            #pragma HLS UNROLL factor=N_TREES
 
-            while(1){
-            #pragma HLS loop_tripcount min=1 max=8
-                tree_data.compact_data = tree[t][node_index];
-                feature_index = tree_data.tree_camps.feature_index;
-                threshold = tree_data.tree_camps.float_int_union.f;
-                node_left = node_index + 1;
-                node_right = tree_data.tree_camps.next_node_right_index;
+                uint8_t node_index = 0;
+                uint8_t node_right;
+                uint8_t node_left;
+                uint8_t feature_index;
+                float threshold;
+                tree_data tree_data;
 
-                node_index = *(int32_t*)&local_features[feature_index] < *(int32_t*)&threshold ? 
-                                        node_left : node_right;
+                while(1){
+                #pragma HLS loop_tripcount min=1 max=8
+                    tree_data.compact_data = tree[t][node_index];
+                    feature_index = tree_data.tree_camps.feature_index;
+                    threshold = tree_data.tree_camps.float_int_union.f;
+                    node_left = node_index + 1;
+                    node_right = tree_data.tree_camps.next_node_right_index;
 
+                    node_index = *(int32_t*)&local_features_ping[feature_index] < *(int32_t*)&threshold ?
+                                            node_left : node_right;
 
-                if (!(tree_data.tree_camps.leaf_or_node & 0x01))
-                    break;
+                    if (!(tree_data.tree_camps.leaf_or_node & 0x01))
+                        break;
+                }
+
+                leaf_value = tree_data.tree_camps.float_int_union.i;;
+                sum += leaf_value;
             }
 
-            leaf_value = tree_data.tree_camps.float_int_union.i;
-            sum += leaf_value;
+            coppy_loop_pong:for (int i = 0; i < N_FEATURE & j < *features_burst_length - 1; i++)
+                local_features_pong[i] = ping_pong & 0x01 ? bram_features_ping[j+1][i] : bram_features_pong[j+1][i];
+
+        }else{
+
+            trees_loop_pong: for (int t = 0; t < N_TREES; t++){
+            #pragma HLS UNROLL factor=N_TREES
+                uint8_t node_index = 0;
+                uint8_t node_right;
+                uint8_t node_left;
+                uint8_t feature_index;
+                float threshold;
+                tree_data tree_data;
+
+                while(1){
+                #pragma HLS loop_tripcount min=1 max=8
+                    tree_data.compact_data = tree[t][node_index];
+                    feature_index = tree_data.tree_camps.feature_index;
+                    threshold = tree_data.tree_camps.float_int_union.f;
+                    node_left = node_index + 1;
+                    node_right = tree_data.tree_camps.next_node_right_index;
+
+                    node_index = *(int32_t*)&local_features_pong[feature_index] < *(int32_t*)&threshold ?
+                                            node_left : node_right;
+
+                    if (!(tree_data.tree_camps.leaf_or_node & 0x01))
+                        break;
+                }
+
+                leaf_value = tree_data.tree_camps.float_int_union.i;;
+                sum += leaf_value;
+            }
+
+            coppy_loop_ping:for (int i = 0; i < N_FEATURE & j < *features_burst_length - 1; i++)
+                local_features_ping[i] = ping_pong & 0x01 ? bram_features_ping[j+1][i] : bram_features_pong[j+1][i];
+
         }
 
         if (ping_pong & 0x01 ){
@@ -68,6 +116,7 @@ void predict(uint64_t tree[N_TREES][N_NODE_AND_LEAFS],
             prediction_pong[j] = sum;
         }
 
+        local_ping_pong = ~local_ping_pong;
     }
     
 }
