1e1a5192d31e6f500044d23948446eb3b23b8c4e
Rodrigo <rodrigo@rodrigo.com>
2024-11-22 17:57:48 +0100

    value leaf -1, 0, 1

--- C_code_vitis/trees_managment.c
+++ C_code_vitis/trees_managment.c
@@ -31,7 +31,7 @@ float generate_threshold(float min, float max,int* seed) {
 
 float generate_leaf_value(int *seed) {
     //float random_f = ((float) 2 * rand_r(seed) / (float)RAND_MAX) - 1.0;
-    int random_f = 2*(rand_r(seed) % 2) - 1;
+    int random_f = (rand_r(seed) % 3) - 1;
 
     return random_f;
 }
